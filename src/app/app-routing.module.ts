f48aff5d29995d9bcba1b9653a33780402366314
daniel_luz <danisluz@gmail.com>
2022-03-04 13:16:21 -0300

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChurchReadComponent } from './features/churches/components/church-read/church-read.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent},
+  { path: "", component: DashboardComponent},
+  { path: "churches", component: ChurchReadComponent},
 ];
 
 @NgModule({
