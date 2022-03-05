25a167bde1b119d61d03ce49e4e86b7e3f9039e9
daniel_luz <danisluz@gmail.com>
2022-03-05 17:02:34 -0300

    content adjustment

--- src/app/features/churches/components/church-read/church-read.component.ts
+++ src/app/features/churches/components/church-read/church-read.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-church-read',
-  templateUrl: './church-read.component.html',
-  styleUrls: ['./church-read.component.scss']
+  templateUrl: './church-read.component.html'
 })
 export class ChurchReadComponent implements OnInit {
 
