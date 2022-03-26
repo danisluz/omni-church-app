045da51bb30cb3133aa5253f0bdb35bc3a514743
daniel_luz <danisluz@gmail.com>
2022-03-26 17:51:42 -0300

    update sector create

--- src/app/pages/sector-create/sector-create.container.ts
+++ src/app/pages/sector-create/sector-create.container.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
   templateUrl: './sector-create.container.html',
 })
 export class SectorCreateContainer implements OnInit {
