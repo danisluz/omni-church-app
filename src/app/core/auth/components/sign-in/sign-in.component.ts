517e540135a860b15b68fad0432d5871da977c0e
danisluz <danisluz@gmail.com>
2022-03-30 10:50:00 -0300

    update sign-in

--- src/app/core/auth/components/sign-in/sign-in.component.ts
+++ src/app/core/auth/components/sign-in/sign-in.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
 
