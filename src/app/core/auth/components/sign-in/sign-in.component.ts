20efbbce82d7010a2abf69393772116b1d69a16c
danisluz <danisluz@gmail.com>
2022-03-30 12:00:02 -0300

    update sign-in

--- src/app/core/auth/components/sign-in/sign-in.component.ts
+++ src/app/core/auth/components/sign-in/sign-in.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
 })
 export class SignInComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor( private formBuilder: FormBuilder) {  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group( {
+      userName: ["", Validators.required ],
+      password: ["", Validators.required ]
+    });
+    console.log(this.loginForm)
   }
-
 }
